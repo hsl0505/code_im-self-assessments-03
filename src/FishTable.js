11f8c3ddf44dc4bfc363c938423f8c2cd58aaa23
hyunsung <hslgood@gmail.com>
2019-12-17 17:33:14 +0900

    assessment03 complete

--- src/FishTable.js
+++ src/FishTable.js
@@ -1,6 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import FishTableRow from "./FishTableRow";
 
 //TODO: FishTable will have many FishTableRows
+
+const FishTable = props => {
+  const arr = props.fishes;
+  const mapArr = arr.map(ele => <FishTableRow key={ele.name} fish={ele} />);
+  return <div>{mapArr}</div>;
+};
 
 export default FishTable;
